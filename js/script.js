8ff163758199d75bd85b6db80e86994e6c7261e4
AngeloDalicandro <angelo.dalicandro1@gmail.com>
2022-06-27 14:55:06 +0200

    structure done

--- js/script.js
+++ js/script.js
@@ -2,9 +2,22 @@ var app = new Vue({
     el: '#root',
 
     data: {
-
+        todos: [
+            {
+                task: 'Leggi per 30 minuti',
+                done: true
+            },
+            {
+                task: 'Chiama la nonna',
+                done: false
+            },
+            {
+                task: 'Porta fuori il cane',
+                done: false
+            }
+        ]
     },
-    
+
     methods: {
 
     }
